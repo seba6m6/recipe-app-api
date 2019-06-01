1521efd3d90d862f441f7c9f9f039ed1ed096be6
Sebastian Meller <batian.meller@gmail.com>
2019-06-01 19:31:40 +0200

    adding tests for custom user model and the model itself

---
+++ app/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, \
+                                        PermissionsMixin
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """creates a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """custom user model that uspports email"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
